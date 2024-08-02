e157aa76a1fc5c907aa1c25e0ac041ad404ba914
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-02 15:02:19 +0700

    menambhakan locator input product name di list product

--- pageObjects/listProduct.page.ts
+++ pageObjects/listProduct.page.ts
@@ -3,6 +3,7 @@ import { Locator, Page } from "@playwright/test";
 export class ListProduct {
     page: Page;
     cardProdcutItem: Locator;
+    inputProductName: Locator;
 
     constructor(page: Page) {
         this.page = page;
