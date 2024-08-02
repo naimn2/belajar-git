fa7d4a2664c3bd30bb197a1e4943e8f666d32483
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-02 14:41:26 +0700

    menambahkan locator input search di home page

--- pageObjects/home.page.ts
+++ pageObjects/home.page.ts
@@ -3,9 +3,11 @@ import { Locator, Page } from '@playwright/test'
 export class HomePage {
     page: Page;
     btnBurger: Locator;
+    inputSearch: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.btnBurger = page.getByTestId('btnBurger');
+        this.inputSearch = page.locator('');
     }
 }
