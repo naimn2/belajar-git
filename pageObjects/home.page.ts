39db0b98351d2d1c4dc60aeb993c0c1a6859012b
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-02 14:40:23 +0700

    menambahkan locator btn profil

--- pageObjects/home.page.ts
+++ pageObjects/home.page.ts
@@ -3,9 +3,11 @@ import { Locator, Page } from '@playwright/test'
 export class HomePage {
     page: Page;
     btnBurger: Locator;
+    btnProfil: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.btnBurger = page.getByTestId('btnBurger');
+        this.btnProfil = page.locator('');
     }
 }
